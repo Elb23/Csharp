de9acaf5e9623a5c2e2f82e161a24344707f4f90
Elb23 <127051694+Elb23@users.noreply.github.com>
2023-09-08 20:39:21 +0300

    Add files via upload

---
+++ Digital Root/Digital Root/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+
+class Program
+{
+    static void DigitalRoot()
+    {
+        do
+        {
+            Console.WriteLine("Please enter a positive number:");
+            string X = Console.ReadLine();
+
+            if (int.TryParse(X, out int userInput) && userInput > 0)
+            {
+                Console.WriteLine($"The Digital Root of {userInput} is: ");
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a positive integer.");
+            }
+
+
+            bool cdt = true;
+
+            while (cdt == true)
+            {
+                List<int> digitList = userInput.ToString().Select(digit => int.Parse(digit.ToString())).ToList();
+                int sum = 0;
+                foreach (int digit in digitList)
+                {
+                    sum += digit;
+                    userInput = sum;
+                }
+                if (userInput < 10)
+                {
+                    Console.WriteLine(userInput);
+                    cdt = false;
+                }
+            }
+        }
+        while (true);
+    }
+    static void Main(string[] args) 
+    {
+
+        DigitalRoot();
+    }
+}
+
+
---
+++ Hourglass/ConsoleApp5/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+class Program
+{
+public static void DrawHourGlass()
+    {
+        do
+        {
+            Console.Write("Hello, Please enter a number bigger than 3:");
+            string userInput = Console.ReadLine();
+            int height;
+            if (int.TryParse(userInput, out height) && height > 3)
+            {
+                List<char> list = new List<char>(new string('*', height).ToCharArray()); ;
+                int lastIndex = (list.Count - 1);
+
+                bool direction = true;
+
+                for (int i = 0; i < lastIndex / 2; i++)
+                {
+                    Console.WriteLine(string.Join("", list));
+                    list[i] = '\u0020'; // whitespace
+                    list[lastIndex - i] = list[i];
+                    direction = false;
+
+                }
+                if (direction == false)
+                {
+                    for (int j = lastIndex / 2; j <= lastIndex; j++)
+                    {
+                        list[j] = '*';
+                        list[lastIndex - j] = '*';
+                        Console.WriteLine(string.Join("", list)); // Output: * * * * *
+                    }
+
+                }
+            }
+            else
+            {
+               
+                Console.WriteLine("Invalid input. Please enter an integer greater than 3."); // Invalid input 
+            }
+        } while (true);
+    }
+
+static void Main()
+{ 
+
+    DrawHourGlass();
+
+}
+
+
+}
